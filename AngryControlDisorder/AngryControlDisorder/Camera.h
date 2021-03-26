778b3534e888364a4c8c9fe43c5159818ea6d3c8
Woo_sj <wsj2681@naver.com>
2021-03-26 22:35:49 +0900

    Camera

--- AngryControlDisorder/AngryControlDisorder/Camera.h
+++ AngryControlDisorder/AngryControlDisorder/Camera.h
@@ -13,6 +13,7 @@ struct VS_CB_CAMERA_INFO
 	XMFLOAT3 position;
 };
 
+class Player;
 
 class Camera
 {
@@ -21,7 +22,6 @@ public:
 	Camera();
 	Camera(const Camera&) = delete;
 	Camera(Camera* camera);
-	Camera& operator=(const Camera&) = delete;
 	virtual ~Camera();
 
 protected:
@@ -42,24 +42,26 @@ protected:
 	float timeLag = 0.f;
 
 	XMFLOAT4X4 view;
-	XMFLOAT4X4 Projection;
+	XMFLOAT4X4 projection;
 
-	D3D12_VIEWPORT viewport;
-	D3D12_RECT sissorRect;
+	D3D12_VIEWPORT viewport{ 0, 0, FRAME_BUFFER_WIDTH , FRAME_BUFFER_HEIGHT, 0.0f, 1.0f };
+	D3D12_RECT scissorRect{ 0, 0, FRAME_BUFFER_WIDTH , FRAME_BUFFER_HEIGHT };
 
 	ID3D12Resource* cbCamera = nullptr;
 	VS_CB_CAMERA_INFO* mappedCamera = nullptr;
 
+	Player* player = nullptr;
+
 public:
 
-	virtual void CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	virtual void CreateShaderVariables(ID3D12Device* device, ID3D12GraphicsCommandList* commandList);
 	virtual void ReleaseShaderVariables();
-	virtual void UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList);
+	virtual void UpdateShaderVariables(ID3D12GraphicsCommandList* commandList);
 
 	void GenerateProjectionMatrix(float fNearPlaneDistance, float fFarPlaneDistance, float fAspectRatio, float fFOVAngle);
 
 	void GenerateViewMatrix();
-	void GenerateViewMatrix(XMFLOAT3 xmf3Position, XMFLOAT3 xmf3LookAt, XMFLOAT3 xmf3Up);
+	void GenerateViewMatrix(const XMFLOAT3& position, const XMFLOAT3& lookAt, const XMFLOAT3& up);
 	void RegenerateViewMatrix();
 
 	virtual void SetViewportsAndScissorRects(ID3D12GraphicsCommandList* pd3dCommandList);
@@ -67,5 +69,52 @@ public:
 	void SetViewport(int xTopLeft, int yTopLeft, int nWidth, int nHeight, float fMinZ = 0.0f, float fMaxZ = 1.0f);
 	void SetScissorRect(LONG xLeft, LONG yTop, LONG xRight, LONG yBottom);
 
+	Player* GetPlayer();
+	void SetPlayer(Player* player);
+
+	DWORD GetMOde();
+	void SetMode(DWORD mode);
+
+	const XMFLOAT3& GetPosition();
+	void SetPosition(const XMFLOAT3& position);
+	void SetPosition(float x, float y, float z);
+
+	const XMFLOAT3& GetRightVector();
+	void SetRightVector(const XMFLOAT3& right);
+	void SetRightVector(float x, float y, float z);
+
+	const XMFLOAT3& GetLookVector();
+	void SetLookVector(const XMFLOAT3& look);
+	void SetLookVector(float x, float y, float z);
+
+	const XMFLOAT3& GetUpVector();
+	void GetUpVector(const XMFLOAT3& up);
+	void GetUpVector(float x, float y, float z);
+
+	const float& GetPitch();
+	void SetPitch(float pitch);
+
+	const float& GetRoll();
+	void SetRoll(float roll);
+
+	const float& GetYaw();
+	void SetYaw(float yaw);
+
+	const XMFLOAT3& GetOffset();
+	void SetOffset(const XMFLOAT3& offset);
+	void SetOffset(float x, float y, float z);
+
+	const float& GetTimeLag();
+	void SetTimeLag(float timeLag);
+
+	const XMFLOAT4X4& GetViewMatrix();
+	const XMFLOAT4X4& GetProjectionMatrix();
+	const D3D12_VIEWPORT& GetViewport();
+	const D3D12_RECT& GetScissorRect();
+
+	virtual void Move(const XMFLOAT3& xmf3Shift);
+	virtual void Rotate(float fPitch = 0.0f, float fYaw = 0.0f, float fRoll = 0.0f);
+	virtual void Update(const XMFLOAT3& xmf3LookAt, float fTimeElapsed);
+	virtual void SetLookAt(const XMFLOAT3& xmf3LookAt);
 
 };
