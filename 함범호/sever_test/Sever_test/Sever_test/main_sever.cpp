9b68c8847d2c073c3729d7a9b9753a4cc41dc63c
double-tiger <gkaqjagh12@naver.com>
2021-04-01 00:37:05 +0900

    Server_closesock

--- 함범호/sever_test/Sever_test/Sever_test/main_sever.cpp
+++ 함범호/sever_test/Sever_test/Sever_test/main_sever.cpp
@@ -4,13 +4,6 @@
 #define MAXTHREAD 5
 
 
-// 상하좌우 상수값 설정
-#define UP 72
-#define DOWN 80
-#define LEFT 75
-#define RIGHT 77
-
-
 
 CRITICAL_SECTION cs;
 // ThreadFunction
@@ -22,13 +15,15 @@ DWORD dwThreadID[MAXTHREAD];
 //스레드 갯수
 static int idIndex = 1;
 
-HANDLE hThread[MAXTHREAD];
+//HANDLE hThread[MAXTHREAD];
+HANDLE hThread;
 int threadCount = 0;
 
 Player_world thread_num_1_player;
 Player_world thread_num_2_player;
 
 
+int thread_empty[MAXTHREAD] = { 0, };
 
 //void err_quit(const char* msg);
 //void err_display(const char* msg);
@@ -37,6 +32,7 @@ int main()
 {
 	int retval = 0;
 
+	wcout.imbue(locale("korean"));
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
@@ -78,13 +74,18 @@ int main()
 			"\n[TCP 서버] 클라이언트 접속: IP 주소= " << inet_ntoa(client_addr.sin_addr) <<
 			", 포트 번호=" << ntohs(client_addr.sin_port) << endl;
 
-		hThread[threadCount] = CreateThread(nullptr, 0, PlayerThread, (LPVOID)client_sock, 0, &dwThreadID[threadCount]);
+		//hThread[threadCount] = CreateThread(nullptr, 0, PlayerThread, (LPVOID)client_sock, 0, &dwThreadID[threadCount]);
+		//threadCount++;
 
-		threadCount++;
+		hThread = CreateThread(nullptr, 0, PlayerThread, (LPVOID)client_sock, 0, NULL);
+
+		if (hThread == NULL)
+			closesocket(client_sock);
+		else { CloseHandle(hThread); }
 	}
 
-	for (auto& i : hThread)
-		CloseHandle(i);
+	/*for (auto& i : hThread)
+		CloseHandle(i);*/
 
 
 	DeleteCriticalSection(&cs);
@@ -99,50 +100,52 @@ DWORD WINAPI PlayerThread(LPVOID arg)
 {
 	int retval;
 	Thread_id thread_id;
-	thread_id.thread_num = idIndex;
-	idIndex++;
+	/*thread_id.thread_num = idIndex;
+	idIndex++;*/
 
-	SOCKET client_sock = (SOCKET)arg;
+	for (int i = 0; i < MAXTHREAD; ++i) {
+		if (thread_empty[i] == 0) {
+			thread_empty[i] = 1;
+			thread_id.thread_num = i;
+				
+		}
+		else {
+
+			//쓰레드 해당개수(5개)를 다 할당했을떄
+		}
+	}
+
+	SOCKET thread_client_sock = (SOCKET)arg;
 	SOCKADDR_IN client_addr;
-	int client_addr_len;
+	int thread_client_addr_len;
 	char buf[2];
 	char GameReady[6];
 	ZeroMemory(&buf, sizeof(buf));
 	
 	Player_world player;
 
-	Player_world other_player;
 
-	
-	
-	char keyboard;
-	
-	////쓰레드 카운터
-	//++thread_count;
-
-
-
-	getpeername(client_sock, (SOCKADDR*)&client_addr, &client_addr_len);
+	getpeername(thread_client_sock, (SOCKADDR*)&client_addr, &thread_client_addr_len);
 
 
 	////송수신/////////////////////////////////////
-	retval = recv(client_sock, (char*)GameReady, sizeof(GameReady), 0);
+	retval = recv(thread_client_sock, (char*)GameReady, sizeof(GameReady), 0);
 	cout << GameReady << "받기 완료" << endl;
 
 	char GameOk[7] = "GameOk";
-	retval = send(client_sock, (char*)GameOk, sizeof(GameOk), 0);
+	retval = send(thread_client_sock, (char*)GameOk, sizeof(GameOk), 0);
 	cout << GameOk << "전송 완료" << endl;
 
 	//id.thread_id =thread_count;
 
 	
 	cout << "lll thread_id = " << thread_id.thread_num << endl;
-	retval = send(client_sock, (char*)&thread_id, sizeof(thread_id), 0);
+	retval = send(thread_client_sock, (char*)&thread_id, sizeof(thread_id), 0);
 	cout << "llll  thread_id = " << thread_id.thread_num << endl;
 	///////////////////////////
 
 	////월드좌표계 초기화
-	XMStoreFloat4x4(&other_player.player_world, XMMatrixIdentity());
+	//XMStoreFloat4x4(&other_player.player_world, XMMatrixIdentity());
 	XMStoreFloat4x4(&player.player_world, XMMatrixIdentity());
 
 	XMStoreFloat4x4(&thread_num_1_player.player_world, XMMatrixIdentity());
@@ -154,113 +157,63 @@ DWORD WINAPI PlayerThread(LPVOID arg)
 		//스레드 아이디 초기화
 		thread_id.thread_num = 0;
 		
-		recv(client_sock, (char*)&thread_id, sizeof(thread_id), 0);
-		recv(client_sock, (char*)&player, sizeof(player), 0);
-
-		/*if (thread_id.thread_num == 1) {
-			
-			thread_num_1_player = player;
-			cout << "thread_1 of thread_2 value = " << thread_id.thread_num << " / " << thread_num_2_player.player_world._41 << " ," << thread_num_2_player.player_world._42 << ", " << thread_num_2_player.player_world._43 << endl;
-
+		retval = recv(thread_client_sock, (char*)&thread_id, sizeof(thread_id), 0);
+		if (retval == SOCKET_ERROR) {
+			display_error("recv : ", WSAGetLastError());
+			break;
 		}
-		else if (thread_id.thread_num == 2) {
-			
-			thread_num_2_player = player;
-			cout << "thread_2 of thread_1 value = " << thread_id.thread_num << " / " << thread_num_1_player.player_world._41 << " ," << thread_num_1_player.player_world._42 << ", " << thread_num_1_player.player_world._43 << endl;
-
-		}*/
-		
-
+		else if (retval == 0)
+			break;
+		retval = recv(thread_client_sock, (char*)&player, sizeof(player), 0);
+		if (retval == SOCKET_ERROR) {
+			display_error("recv : ", WSAGetLastError());
+			break;
+		}
+		else if (retval == 0)
+			break;
 		
 
 		EnterCriticalSection(&cs);
 		
-		if (thread_id.thread_num == 1) {
-			//retval = recv(client_sock, (char*)&player, sizeof(player), 0);
-			//cout << player.player_world._41 << " ," << player.player_world._42 << ", " << player.player_world._43 << endl;
-
-			//cout <<"thread_id = " <<thread_id.thread_num <<" / "<<other_player.player_world._41 << "/ " << other_player.player_world._42 << "/ " << other_player.player_world._43 << endl;
-
-			
-			thread_num_1_player = player;
-			send(client_sock, (char*)&thread_num_2_player, sizeof(thread_num_2_player), 0);
-			cout << "thread_1 of thread_2 value = " << thread_id.thread_num << " / " << thread_num_2_player.player_world._41 << " ," << thread_num_2_player.player_world._42 << ", " << thread_num_2_player.player_world._43 << endl;
+		if (thread_id.thread_num == 1 || thread_id.thread_num == 3) {
 		
-			//cout << other_player.player_world._41 << ", " << other_player.player_world._42 << ", " << other_player.player_world._43 << endl;
+			thread_num_1_player = player;
+			retval = send(thread_client_sock, (char*)&thread_num_2_player, sizeof(thread_num_2_player), 0);
+			cout << "thread_1 of thread_2 value = " << thread_id.thread_num << " / " << thread_num_2_player.player_world._41 << 
+				" ," << thread_num_2_player.player_world._42 << ", " << thread_num_2_player.player_world._43 << endl;
+		
 			
-			//cout << "thread_id = " << thread_id.thread_num << " / " << thread_num_2_player.player_world._41 << " ," << thread_num_2_player.player_world._42 << ", " << thread_num_2_player.player_world._43 << endl;
-
 
 		}
-		else if (thread_id.thread_num == 2) {
+		else if (thread_id.thread_num == 2 || thread_id.thread_num == 4) {
 			
-			//retval = recv(client_sock, (char*)&other_player, sizeof(other_player), 0);
-			//cout << other_player.player_world._41 << ", " << other_player.player_world._42 << ", " << other_player.player_world._43 << endl;
-
-			//cout << "thread_id = " << thread_id.thread_num <<" / "<< player.player_world._41 << " ," << player.player_world._42 << ", " << player.player_world._43 << endl;
-
 			
 			thread_num_2_player = player;
-			send(client_sock, (char*)&thread_num_1_player, sizeof(thread_num_1_player), 0);
-			//cout << "thread_id = " << thread_id.thread_num << " / " << thread_num_1_player.player_world._41 << " ," << thread_num_1_player.player_world._42 << ", " << thread_num_1_player.player_world._43 << endl;
-			cout << "thread_2 of thread_1 value = " << thread_id.thread_num << " / " << thread_num_1_player.player_world._41 << " ," << thread_num_1_player.player_world._42 << ", " << thread_num_1_player.player_world._43 << endl;
+			retval = send(thread_client_sock, (char*)&thread_num_1_player, sizeof(thread_num_1_player), 0);
+			cout << "thread_2 of thread_1 value = " << thread_id.thread_num << " / " << thread_num_1_player.player_world._41 << 
+				" ," << thread_num_1_player.player_world._42 << ", " << thread_num_1_player.player_world._43 << endl;
 
 		
 		}
 
 		LeaveCriticalSection(&cs);
 		
-		
-
-		/*cout << other_player.player_world._41 << ", " << other_player.player_world._42 << ", " << other_player.player_world._43 << endl;
-		cout << player.player_world._41 << " ," << player.player_world._42 << ", " << player.player_world._43 << endl;*/
-
-
-
-		//////월드좌표계 초기화
-		//XMStoreFloat4x4(&other_player.player_world, XMMatrixIdentity());
-
-		////서버에서 직접 키보드 입력 받기 
-		//if (_kbhit()) {        //키보드 입력 확인 (true / false)
-		//	keyboard = _getch();      // 방향키 입력시 224 00이 들어오게 되기에 앞에 있는 값 224를 없앰
-		//	if (keyboard == -32) {    // -32로 입력되면
-		//		keyboard = _getch();  // 새로 입력값을 판별하여 상하좌우 출력
-		//		switch (keyboard) {
-		//		case LEFT:
-		//			other_player.player_world._41 -= 1.0f;
-		//			printf("왼쪽으로 이동\n");
-		//			break;
-		//		case RIGHT:
-		//			other_player.player_world._41 += 1.0f;
-		//			printf("오른쪽으로 이동\n");
-		//			break;
-		//		case UP:
-		//			other_player.player_world._43 += 1.0f;
-		//			printf("위로 이동\n");
-		//			break;
-		//		case DOWN:
-		//			other_player.player_world._43 -= 1.0f;
-		//			printf("아래로 이동\n");
-		//			break;
-		//		}
-		//	}
-		//}
-
-		////값보내기
-		//retval = send(client_sock, (char*)&other_player, sizeof(other_player), 0);
-		//cout << other_player.player_world._41 << " " << other_player.player_world._42 << " " << other_player.player_world._43 << endl;
-		//
-		
-
 
 	}
-
+	closesocket(thread_client_sock);
 
 	return 0;
 }
 
 
-
+void display_error(const char* msg, int err_no)
+{
+	WCHAR* lpMsgBuf;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, err_no, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+	cout << msg;
+	wcout << lpMsgBuf << endl;
+	LocalFree(lpMsgBuf);
+}
 
 void err_quit(char* msg)
 {
